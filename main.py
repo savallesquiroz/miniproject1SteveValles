a2c5333cd7d22ebf53604e1b7b7a626b11af93f2
Steve Valles <savallesquiroz@mail.fhsu.edu>
2024-02-10 13:47:52 +0100

    same 5 stock tickers added in main.py but in a single line. Simpler code.

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
-#(5/5 points) Initial comments with your name, class and project at the top of your .py file.
+# (5/5 points) Initial comments with your name, class and project at the top of your .py file.
 
 # INF601 - Advanced Programming in Python
 # Steve Valles Quiroz
 # Mini Project 1
 
-#(5/5 points) Proper import of packages used.
+# (5/5 points) Proper import of packages used.
 import numpy as np
 import matplotlib.pyplot as plt
 import yfinance as yf
 
-#(20/20 points) Using an API of your choice (yfinance works), collect the closing price of 5 of your favorite stock tickers for the last 10 trading days.
-nvidia = yf.Ticker("NVDA")
-intel = yf.Ticker("INTC")
-nike = yf.Ticker("NKE")
-amazon = yf.Ticker("AMZN")
-netflix = yf.Ticker("NFLX")
+# (20/20 points) Using an API of your choice (yfinance works), collect the closing price of 5 of your favorite stock tickers for the last 10 trading days.
+tickers = yf.Tickers('nvda intc nke amzn nflx')
 
-#(10/10 points) Store this information in a list that you will convert to a array in NumPy.
-#(10/10 points) Plot these 5 graphs. Feel free to add as much information to the graphs as you like exploring the documentation for matplotlib. At minimum it just needs to show 10 data points.
-#(10/10 points) Save these graphs in a folder called charts as PNG files. Do not upload these to your project folder, the project should save these when it executes. You may want to add this folder to your .gitignore file.
-#(10/10 points) There should be a minimum of 5 commits on your project, be sure to commit often!
-#(10/10 points) I will be checking out the main branch of your project. Please be sure to include a requirements.txt file which contains all the packages that need installed. You can create this fille with the output of pip freeze at the terminal prompt.
-#(20/20 points) There should be a README.md file in your project that explains what your project is, how to install the pip requirements, and how to execute the program. Please use the GitHub flavor of Markdown.
+# (10/10 points) Store this information in a list that you will convert to a array in NumPy.
+# (10/10 points) Plot these 5 graphs. Feel free to add as much information to the graphs as you like exploring the documentation for matplotlib. At minimum it just needs to show 10 data points.
+# (10/10 points) Save these graphs in a folder called charts as PNG files. Do not upload these to your project folder, the project should save these when it executes. You may want to add this folder to your .gitignore file.
+# (10/10 points) There should be a minimum of 5 commits on your project, be sure to commit often!
+# (10/10 points) I will be checking out the main branch of your project. Please be sure to include a requirements.txt file which contains all the packages that need installed. You can create this fille with the output of pip freeze at the terminal prompt.
+# (20/20 points) There should be a README.md file in your project that explains what your project is, how to install the pip requirements, and how to execute the program. Please use the GitHub flavor of Markdown.
