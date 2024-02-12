41209ec1396e2495d298c46522574a5c6e0f3084
Steve Valles <savallesquiroz@mail.fhsu.edu>
2024-02-12 13:22:27 +0100

    mytickers10days converted into a list to mytickersList

--- main.py
+++ main.py
@@ -8,11 +8,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import yfinance as yf
+import pandas as pd
 
 # (20/20 points) Using an API of your choice (yfinance works), collect the closing price of 5 of your favorite stock tickers for the last 10 trading days.
 tickers = yf.Tickers('nvda intc nke amzn nflx')
 mytickers10days = tickers.history(period='10d', interval='1d')
-# (10/10 points) Store this information in a list that you will convert to a array in NumPy.
+# (10/10 points) Store this information in a list that you will convert to an array in NumPy.
+mytickersList = mytickers10days.to_numpy().tolist()
 # (10/10 points) Plot these 5 graphs. Feel free to add as much information to the graphs as you like exploring the documentation for matplotlib. At minimum it just needs to show 10 data points.
 # (10/10 points) Save these graphs in a folder called charts as PNG files. Do not upload these to your project folder, the project should save these when it executes. You may want to add this folder to your .gitignore file.
 # (10/10 points) There should be a minimum of 5 commits on your project, be sure to commit often!
